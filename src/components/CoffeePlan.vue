ed0f0f3b0355d9b42294cde5b57bd081c9cc4933
mardbrito <marcelo4web@gmail.com>
2024-04-29 19:03:32 -0400

    Communication between components with custom events

--- src/components/CoffeePlan.vue
+++ src/components/CoffeePlan.vue
@@ -1,11 +1,18 @@
 <script setup>
-defineProps({
-  name: { type: String, default: 'Default Plan' }
+const props = defineProps({
+  name: { type: String, default: 'Default Plan' },
+  selected: { type: Boolean, default: false }
 })
+
+const emit = defineEmits(['selected'])
+
+function selectPlan() {
+  emit('selected', props.name)
+}
 </script>
 
 <template>
-  <div class="plan">
+  <div class="plan" @click="selectPlan" :class="{ 'active-plan': selected }">
     <div class="description">
       <span class="title"> {{ name }} </span>
     </div>
--- src/components/PlanPicker.vue
+++ src/components/PlanPicker.vue
@@ -2,10 +2,23 @@
 import CoffeePlan from '../components/CoffeePlan.vue'
 import { ref } from 'vue'
 const plans = ref(['The Single', 'The Curious', 'The Addict', 'The Hacker'])
+
+const selectCoffeePlan = ref()
+
+function handleSelectedCoffeePlan(name) {
+  selectCoffeePlan.value = name
+}
 </script>
 
 <template>
   <div class="plans">
-    <CoffeePlan v-for="plan in plans" :key="plan" :name="plan" />
+    {{ selectCoffeePlan }}
+    <CoffeePlan
+      v-for="plan in plans"
+      :key="plan"
+      :name="plan"
+      :selected="plan === selectCoffeePlan"
+      @selected="handleSelectedCoffeePlan"
+    />
   </div>
 </template>
